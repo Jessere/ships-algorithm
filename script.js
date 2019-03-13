f63c351c21a96cd16ac94ac7dc4712e727cf14db
Konstantin Netesov <netesov.jezz@gmail.com>
2019-03-13 10:54:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$Document.ready(function){
+
+  $(".actionButton").click(function(
+    int n = $(".n").getValue();
+    int m = $(".m").getValue();
+
+    for (int i=0; i < n; i++){
+
+
+
+    }
+
+  ));
+
+
+}
